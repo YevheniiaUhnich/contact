05efcf5cc3ab72adfe77c03b3b0107e55166a0ed
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-02-22 15:49:15 +0200

    update

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
-import { LS_KEY } from './refs';
+import { LS_KEY } from './refs.js';
 
 const token = localStorage.getItem(LS_KEY);
 axios.defaults.baseURL = 'https://connections-api.goit.global/';
 axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+export async function registerUserService(user) {
+    try{
+const {data} = await axios.post('/users/signup', user);
+return data;
+    } catch(error){
+        console.log(error.message);
+    }
+    
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import { registerUserService } from "./js/api.js";
 
+const loginForm = document.querySelector(".login-form");
+const registerBtn = document.querySelector(".sign-up-btn");
+
+
+async function registerUser(event) {
+    event.preventDefault();
+    const { email, password, name} = event.currentTarget.elements;
+     const response = await registerUserService({
+        email: email.value,
+        password: password.value,
+        name: name.value
+    })
+    console.log(response);
+    localStorage.setItem('token', response.token);
+}
+
+function openModal() {
+instanceRegister.show();
+const registerForm = document.querySelector(".register-form");
+registerForm.addEventListener("submit", registerUser)
+}
+
+const instanceRegister = basicLightbox.create(
+    document.querySelector('#register')
+  );
+
+  registerBtn.addEventListener("click", openModal)
